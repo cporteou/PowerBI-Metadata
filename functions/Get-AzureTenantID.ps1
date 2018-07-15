281ee17455d3cc0f799825ddfed2291aa70cd21d
Paul Broadwith <pauby@users.noreply.github.com>
2018-07-15 20:37:38 +0100

    Moved function params into their own sets and refactored code for that

--- functions/Get-AzureTenantID.ps1
+++ functions/Get-AzureTenantID.ps1
@@ -22,32 +22,29 @@ Get-AzureTenantID -Email 'Craig@craigporteous.com'
 #>
 function Get-AzureTenantID{
 
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = 'Domain')]
     param
     (         
+        [Parameter(ParameterSetName = 'Domain')]
         [string]
         $Domain,
  
+        [Parameter(ParameterSetName = 'Email')]
         [string]
+        [ValidatePattern('.+\@.+\..+')]
         $Email
     )
 
     Process{
         try {
-            if($Domain){
-                Write-Verbose 'Domain provided.'
-            }
-            elseif ($Email) {
-                Write-Verbose 'Split the string on the username to get the Domain.'
+            if ($Email) {
+                Write-Verbose 'Email address passed. Extract the domain.'
                 $Domain = $Email.Split("@")[1]    
             }
-            else{
-                throw
-                Write-Warning 'You must provide a valid Domain or User email to proceed.'
-            }
-            
+
+            Write-Verbose "Domain being used: '$Domain'"
             Write-Verbose 'Query Azure anonymously (this may not work for ALL tenant domains. Eg. Those that use .onmicrosoft.com)'
-            $tenantID = (Invoke-WebRequest -UseBasicParsing https://login.windows.net/$($Domain)/.well-known/openid-configuration|ConvertFrom-Json).token_endpoint.Split('/')[3]
+            $tenantID = (Invoke-WebRequest -UseBasicParsing https://login.windows.net/$($Domain)/.well-known/openid-configuration | ConvertFrom-Json).token_endpoint.Split('/')[3]
         
         }
         catch {
