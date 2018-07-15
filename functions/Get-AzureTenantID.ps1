8d5b1e78bb376c8f1e32c621a1bd3bcb800d984e
Paul Broadwith <pauby@users.noreply.github.com>
2018-07-15 20:39:34 +0100

    removed whitespace

--- functions/Get-AzureTenantID.ps1
+++ functions/Get-AzureTenantID.ps1
@@ -3,7 +3,7 @@
 This function returns the Azure Tenant ID for a provided domain
 
 .DESCRIPTION
-This function queries Azure anonymously to return the provided domain's 
+This function queries Azure anonymously to return the provided domain's
 tenant ID. The function can take a valid user email for that domain or
 the domain name itself.
 
@@ -24,11 +24,11 @@ function Get-AzureTenantID{
 
     [CmdletBinding(DefaultParameterSetName = 'Domain')]
     param
-    (         
+    (
         [Parameter(ParameterSetName = 'Domain')]
         [string]
         $Domain,
- 
+
         [Parameter(ParameterSetName = 'Email')]
         [string]
         [ValidatePattern('.+\@.+\..+')]
@@ -39,13 +39,12 @@ function Get-AzureTenantID{
         try {
             if ($Email) {
                 Write-Verbose 'Email address passed. Extract the domain.'
-                $Domain = $Email.Split("@")[1]    
+                $Domain = $Email.Split("@")[1]
             }
 
             Write-Verbose "Domain being used: '$Domain'"
             Write-Verbose 'Query Azure anonymously (this may not work for ALL tenant domains. Eg. Those that use .onmicrosoft.com)'
             $tenantID = (Invoke-WebRequest -UseBasicParsing https://login.windows.net/$($Domain)/.well-known/openid-configuration | ConvertFrom-Json).token_endpoint.Split('/')[3]
-        
         }
         catch {
             throw $_
